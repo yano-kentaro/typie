f9a7ebfb7edd77f397ad492f74e69ad8b1aaa6f8
yano-kentaro <ynkn0829@gmail.com>
2021-02-17 22:43:22 +0900

    マイページブラッシュアップ

--- app/javascript/Books.vue
+++ app/javascript/Books.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app id="books">
-    <v-btn href="/users/sign_out" method="delete">ログアウト</v-btn>
+    <div class="bg-img-books">
+
+    </div>
   </v-app>
 </template>
 
@@ -8,11 +10,18 @@
 import axios from 'axios';
 
 export default {
-  methods: {
-    logOut: function() {
-      axios.delete('/users/sign_out')
-      .then
-    }
-  }
 }
 </script>
+
+<style scoped>
+.bg-img-books {
+  background-image: url(/computer-1209641_1920.jpg);
+  min-height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
