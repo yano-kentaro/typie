8241ebcdda43c8a974522e93abab76cd9c0bbd56
yano-kentaro <ynkn0829@gmail.com>
2021-02-21 13:39:05 +0900

    webpackによるjquery導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,6 +4,15 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = environment
 
 environment.config.merge({
