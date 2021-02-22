59d4101931436d69e462d2e14c995943ed5538b2
yano-kentaro <ynkn0829@gmail.com>
2021-02-22 11:26:40 +0900

    TypingのFlag作成

--- app/javascript/Books.vue
+++ app/javascript/Books.vue
@@ -18,7 +18,13 @@
             {{showBook.title}} #{{showBook.language}}
           </v-card-title>
           <v-card-actions>
-            <v-btn color="red" dark @click="toggleDeleteModal(showBook.id)">Delete</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="red" dark @click="toggleShowDialog(showBook.id)">cancel</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="red" dark @click="toggleDeleteDialog(showBook.id)">Delete</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="toggleTypingModal(showBook.id)">Typing</v-btn>
+            <v-spacer></v-spacer>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -34,9 +40,22 @@
           </v-card-text>
           <v-divider></v-divider>
           <v-card-actions>
-            <v-btn color="red" dark @click="deleteBook()">
-              Delete
-            </v-btn>
+            <v-btn color="red" dark @click="toggleDeleteDialog(showBook.id)">cancel</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="red" dark @click="deleteBook(showBook.id)">Delete</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="modalTypingFlag" width="1000" persistent>
+        <v-card>
+          <v-card-title class="headline primary white--text">Typing Challenge</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>{{showBook.title}}</v-card-text>
+          <v-card-actions>
+            <v-btn color="red" dark @click="toggleTypingModal(showBook.id)">cancel</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="beginTyping()">start</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -56,6 +75,8 @@ export default {
       dialogShowFlag: false,
       showBook: "showBook",
       dialogDeleteFlag: false,
+      typingWords: "typingWords",
+      modalTypingFlag: false,
     }
   },
   mounted() {
@@ -66,7 +87,7 @@ export default {
       axios.get('/api/books')
       .then(response=>(
         this.indexBooks = response.data
-      ))
+      ));
     },
     toggleShowDialog: function(id) {
       axios.get('/api/books/' + id)
@@ -80,16 +101,26 @@ export default {
       axios.delete('/api/books/' + this.id)
       .then(response=> {
         this.setBook();
-      })
+      });
       this.dialogDeleteFlag = !this.dialogDeleteFlag
       this.dialogShowFlag = !this.dialogShowFlag
     },
-    toggleDeleteModal: function(id) {
+    toggleDeleteDialog: function(id) {
       this.id = id
       this.dialogDeleteFlag = !this.dialogDeleteFlag
-    }
+    },
+    toggleTypingModal: function(id) {
+      this.id = id
+      this.modalTypingFlag = !this.modalTypingFlag
+      this.dialogShowFlag = !this.dialogShowFlag
+    },
   }
 }
+      // axios.typing('/api/books/' + this.id)
+      // .then(response=> {
+      //   this.typingWords = response.data
+      // });
+
 </script>
 
 <style scoped>
@@ -127,6 +158,14 @@ export default {
 
 .book:hover {
   transform: scale(1.1, 1.1);
+}
+
+button {
+  transition: all .2s ease;
+}
+
+button:hover {
+  transform: scale(1.15, 1.15);
 }
 
 </style>
