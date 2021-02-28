b1e2a1df6b71e696b7a250992fcecf5044d5e211
yano-kentaro <ynkn0829@gmail.com>
2021-02-28 11:31:05 +0900

    Capistranoを用いた自動デプロイの設定

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.15.0'
+
+set :application, 'typie'
+
+set :repo_url,  'git@github.com:yano-kentaro/typie.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/ynkn1995.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
