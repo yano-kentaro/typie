bb1fed4ac04baf3c5c9f6f9a4ba9c51014245968
yano-kentaro <ynkn0829@gmail.com>
2021-03-10 18:26:00 +0900

    scoreモデルの単体テスト

--- spec/factories/scores.rb
+++ spec/factories/scores.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :score do
-    
+    typing_score { "120" }
+    typing_time  { "12.00" }
   end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Score, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'スコアの新規登録' do
+    before do
+      @user = FactoryBot.create(:user)
+      @book = FactoryBot.build(:book)
+      @book.user_id = @user.id
+      @book.save
+      @score = FactoryBot.build(:score)
+      @score.book_id = @book.id
+    end
+    context 'スコアの新規登録が出来る' do
+      it '過不足なくデータが送信されている' do
+        expect(@score).to be_valid
+      end
+    end
+  end
 end
